4962341c98cf128ce2511dbc55450bc766406183
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-10-25 18:18:11 +0300

    3-aggregator/fix

--- telemetry/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApp.java
+++ telemetry/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApp.java
@@ -1,9 +1,13 @@
 package ru.practicum.aggregator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import ru.practicum.aggregator.configuration.AppKafkaProperties;
+import ru.practicum.collector.CollectorApp;
 
 @EnableConfigurationProperties(AppKafkaProperties.class)
 @SpringBootApplication(scanBasePackages = {
@@ -14,5 +18,11 @@ import ru.practicum.aggregator.configuration.AppKafkaProperties;
 public class AggregatorApp {
     public static void main(String[] args) {
         SpringApplication.run(AggregatorApp.class, args);
+
+        ConfigurableApplicationContext collectorCtx =
+                new SpringApplicationBuilder(CollectorApp.class)
+                        .web(WebApplicationType.NONE)
+                        .properties("grpc.server.port: 59091")
+                        .run(args);
     }
 }
