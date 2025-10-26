3ecdc42185a7902f2e9db3d451ed96cba85887dc
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-10-26 18:10:06 +0300

    4-analyzer/fix

--- telemetry/analyzer/src/main/java/ru/practicum/analyzer/repository/ActionRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.analyzer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.analyzer.model.Action;
-
-public interface ActionRepository extends JpaRepository<Action, Long> {
-}
